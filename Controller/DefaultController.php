cb79b69c59ded5059b13b8b9e1004c7e4c152f45
Maci <info@macister.com>
2014-10-08 03:29:17 +0200

    general edits

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,8 +6,4 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('MaciTranslatorBundle:Default:index.html.twig');
-    }
 }
