d635d2bf8c4c013e2ca0b487e6722bca698711cc
Maci <info@macister.com>
2015-03-02 01:05:10 +0100

    general edits

--- Form/Type/LanguageType.php
+++ Form/Type/LanguageType.php
@@ -25,7 +25,8 @@ class LanguageType extends AbstractType
 			->add('translations', 'a2lix_translations', array(
                 'fields' => array(
                     'text' => array(
-                        'required' => false
+                        'required' => false,
+                        'attr' => array('class'=>'noeditor')
                     )
                 )
             ))
